487dbded208f6ff426b2134a570ae0a1b3aa1b74
MariiiomH <52500501+MariiiomH@users.noreply.github.com>
2020-09-14 21:54:07 +0200

    Add files via upload

---
+++ Demo AR Game/IKControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKControl : MonoBehaviour
+{
+
+    protected Animator animator;
+
+    public bool ikActive = false;
+    public Transform HandObj = null;
+    public Transform lookObj = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+
+            //if the IK is active, set the position directly to the goal. 
+            if (ikActive)
+            {
+
+                // Set the look target position, if one has been assigned
+                if (lookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+
+                // Set both the right & left hand  target position , if one has been assigned
+                if (HandObj != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                    animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+         
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, HandObj.position);
+                    animator.SetIKPosition(AvatarIKGoal.LeftHand,  HandObj.position);
+              
+                    //animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
+                }
+
+            }
+
+            //if the IK is not active, set the position and of the hand and head back to the original position
+            else
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
---
+++ Demo AR Game/pick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pick : MonoBehaviour {
+  
+
+    public Transform skele;  //skeletonObj
+    Rigidbody rb;            //GoldCoin    
+
+    public GameObject skele_child , Marker_skel_parent ; 
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        //Move Rigidbody with skeleObj
+        rb.MovePosition(skele.position + skele.forward * Time.deltaTime);
+
+        //Move skeleObj Toward his Marker (Follow his parent)
+        skele_child.transform.parent = Marker_skel_parent.transform;
+        skele_child.transform.position = Marker_skel_parent.transform.position;
+
+        //Move Both Rigidbody & skeleObj Toward The Marker of Skele
+        rb.MovePosition(skele_child.transform.position + skele.forward * Time.deltaTime);
+       
+        //to stop GoldCoin from movement 
+        rb.velocity = Vector3.zero;
+         
+
+
+    }
+
+
+
+}
+
